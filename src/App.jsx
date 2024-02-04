2642ccf9460eecd6ccdde57c7f2cb11d9d5c4a71
chiku <codexchiku@gmail.com>
2024-02-05 01:00:13 +0530

    first commit of my app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import {Header}  from "./components/Header.jsx"
+import { Balance } from "./components/Balance.jsx"
+import { IncomeExpense } from "./components/IncomeExpense.jsx"
+import { TransactionList } from "./components/TransactionList.jsx"
+import { AddTransaction } from "./components/AddTransaction.jsx"
+
+import { GlobalProvider } from "./context/GlobalState.jsx"
+import './App.css'
+
+function App() {
+  
+  return (
+    <GlobalProvider>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpense/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
+    </GlobalProvider>
+  )
+}
+
+export default App
